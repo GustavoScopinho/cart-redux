a9d8d9985c1411a8749a0b10552f95d75170a9fd
Gustavo Scopinho <102439841+GustavoScopinho@users.noreply.github.com>
2022-12-15 19:59:58 -0300

    Adicionando função de adicionar e remover do carrinho

--- src/components/ConteudoHome/ConteudoHome.jsx
+++ src/components/ConteudoHome/ConteudoHome.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { cartSlice } from '../../data/cartSlice'
+
 import Card from '@mui/material/Card'
 import CardActions from '@mui/material/CardActions'
 import CardContent from '@mui/material/CardContent'
@@ -9,12 +12,19 @@ import productList from '../../data/productList.json'
 import styles from './ConteudoHome.module.css'
 
 export const ConteudoHome = () => {
+  const { cartProductIds } = useSelector(state => state.cart)
+  const { addToCart, removeFromCart } = cartSlice.actions
+
+  const dispatch = useDispatch()
+
+  console.log(cartProductIds)
+
   return (
     <>
       <div className={styles.ContainerHome}>
         {productList.products.map(product => {
           return (
-            <Card sx={{ width: 400 }}>
+            <Card sx={{ width: 400 }} key={product.id}>
               <CardMedia
                 component="img"
                 height="140"
@@ -32,6 +42,20 @@ export const ConteudoHome = () => {
               <CardActions>
                 <Button size="small">ID: {product.id}</Button>
                 <Button size="small">PRICE: ${product.price}</Button>
+                {!cartProductIds.includes(product.id) && (
+                  <Button
+                    size="small"
+                    onClick={() => dispatch(addToCart(product.id))}
+                  >
+                    Add to cart
+                  </Button>
+                )}
+                <Button
+                  size="small"
+                  onClick={() => dispatch(removeFromCart(product.id))}
+                >
+                  Remove from cart
+                </Button>
               </CardActions>
             </Card>
           )
